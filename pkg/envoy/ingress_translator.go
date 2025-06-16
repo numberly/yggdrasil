985100c16aef2a9afc85797115fbf2f7961d0b2a
Nicolas Puffay <nicolas.puffay@numberly.com>
2025-06-16 12:13:38 +0200

    feat: changing condition of ecdsa maximum support from p-256 to p-521

--- pkg/envoy/ingress_translator.go
+++ pkg/envoy/ingress_translator.go
@@ -353,8 +353,8 @@ func validateTlsSecret(secret *v1.Secret) (bool, error) {
 		if !ok {
 			return false, fmt.Errorf("error in *ecdsa.PublicKey type assertion")
 		}
-		if ecdsaPub.Curve.Params().BitSize > 256 {
-			logrus.Infof("skipping ECDSA %s certificate %s/%s: only P-256 certificates are supported", ecdsaPub.Curve.Params().Name, secret.Namespace, secret.Name)
+		if ecdsaPub.Curve.Params().BitSize > 521 {
+			logrus.Infof("skipping ECDSA %s certificate %s/%s: only P-256, P-384 and P-521 certificates are supported", ecdsaPub.Curve.Params().Name, secret.Namespace, secret.Name)
 			return false, nil
 		}
 	}
